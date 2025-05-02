35eda89d66307ab62114d36218c8646d630cf769
João Gabriel <joaogabriel.trindade04@gmail.com>
2025-05-02 13:07:23 -0300

    Adicionando variaveis iniciais na classe aluguel

--- models/aluguel.java
+++ models/aluguel.java
@@ -1,4 +1,10 @@
 package models;
 
+import java.util.Date;
+
 public class aluguel {
+    private int id;
+    private Date dataAluguel;
+    private Date devolucaoAluguel;
+    private String status;
 }
