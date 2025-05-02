f4cba805cd42b20969a94a998a38e818bb696518
João Gabriel <joaogabriel.trindade04@gmail.com>
2025-05-02 12:34:40 -0300

    Adicionando variaveis iniciais na classe livro

--- models/livro.java
+++ models/livro.java
@@ -1,4 +1,9 @@
 package models;
 
 public class livro {
+    private String titulo;
+    private String editora;
+    private int anoPublicacao;
+    private String autor;
+    private int exemplares;
 }
