f83b57eb85a6eaabba8fc534b7ffb84ccc631174
João Gabriel <joaogabriel.trindade04@gmail.com>
2025-05-03 15:18:34 -0300

    Adicionei Getters, construtores e métodos na classe usuário

--- models/usuario.java
+++ models/usuario.java
@@ -6,4 +6,32 @@ public class usuario {
     private int id;
     private String telefone;
     private int livrosAlugados;
+
+    //Getters
+    public String getNome() {return nome;}
+    public String getEmail() {return email;}
+    public int getId() {return id;}
+    public String getTelefone() {return telefone;}
+    public int getLivrosAlugados() {return livrosAlugados;}
+
+    //Construtor
+    public usuario(String nome, String email, int id, String telefone, int livrosAlugados) {
+        this.nome = nome;
+        this.email = email;
+        this.id = id;
+        this.telefone = telefone;
+        this.livrosAlugados = livrosAlugados;
+    }
+
+    //metodos para controle de livros
+    public void adicionarLivros() {
+        livrosAlugados++;
+    }
+    public void removerLivros() {
+        if (livrosAlugados > 0) {
+            livrosAlugados--;
+        }
+    }
+
+
 }
