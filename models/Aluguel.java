4fd1df07f57622464d0598d088654f352ef4c956
joa576 <joao.mcruz@souunit.com.br>
2025-05-04 23:18:31 -0300

    Adicionei calcularMulta()

--- models/Aluguel.java
+++ models/Aluguel.java
@@ -1,10 +1,24 @@
 package models;
 
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class Aluguel {
     private int id;
     private Date dataAluguel;
     private Date devolucaoAluguel;
     private String status;
+
+    public void calcularMulta(){
+    Date hoje = new Date();
+
+        if (hoje.after(devolucaoAluguel)) {
+            long diferencaMillis = hoje.getTime() - devolucaoAluguel.getTime();
+            long diasAtraso = TimeUnit.DAYS.convert(diferencaMillis, TimeUnit.MILLISECONDS);
+            double valorMulta = diasAtraso * 5.0;
+            System.out.println("Multa por atraso: R$ " + valorMulta);
+        } else {
+            System.out.println("Nenhuma multa. A devolução está no prazo.");
+        }
+    }
 }
